90e91e0003a4ac2b54f09d353c2cfe2ea45cb763
Bhupesh1x <bhupeshvyas202@gmail.com>
2025-09-14 17:44:48 +0530

    Convex package setup and basic example of schema and it's integration with next app

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,15 +1,17 @@
-import { add } from "@workspace/math/add";
-import { Input } from "@workspace/ui/components/input";
-import { Button } from "@workspace/ui/components/button";
+"use client";
+
+import { useQuery } from "convex/react";
+
+import { api } from "@workspace/backend/_generated/api";
 
 export default function Page() {
+  const data = useQuery(api.users.getMany);
+
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello apps/web</h1>
-        <Button size="sm">Button</Button>
-        <p>{add(2, 3)}</p>
-        <Input />
+        {JSON.stringify(data, null, 2)}
       </div>
     </div>
   );
