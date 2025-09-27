7bb9502acc9f459ec854278c4cf0c0b75b0a1366
Bhupesh1x <bhupeshvyas202@gmail.com>
2025-09-28 00:57:39 +0530

    Added getOne conversation query

--- packages/backend/convex/public/conversations.ts
+++ packages/backend/convex/public/conversations.ts
@@ -1,6 +1,6 @@
 import { ConvexError, v } from "convex/values";
 
-import { mutation } from "../_generated/server";
+import { mutation, query } from "../_generated/server";
 
 export const create = mutation({
   args: {
@@ -28,5 +28,34 @@ export const create = mutation({
     });
 
     return conversationId;
+  },
+});
+
+export const getOne = query({
+  args: {
+    sessionId: v.id("contactSessions"),
+    conversationId: v.id("conversations"),
+  },
+  handler: async (ctx, args) => {
+    const session = await ctx.db.get(args.sessionId);
+
+    if (!session || session?.expiresAt < Date.now()) {
+      throw new ConvexError({
+        code: "Unauthorized",
+        message: "Invalid session",
+      });
+    }
+
+    const conversation = await ctx.db.get(args.conversationId);
+
+    if (!conversation) {
+      return null;
+    }
+
+    return {
+      _id: conversation._id,
+      status: conversation.status,
+      threadId: conversation.threadId,
+    };
   },
 });
