f37572cbfc7cc114945756b1ac18f0a960fbea0b
Bhupesh1x <bhupeshvyas202@gmail.com>
2025-10-11 13:30:59 +0530

    Added new table for widget settings

--- packages/backend/convex/schema.ts
+++ packages/backend/convex/schema.ts
@@ -51,4 +51,17 @@ export default defineSchema({
   })
     .index("by_organization_id", ["organizationId"])
     .index("by_organization_id_and_service", ["organizationId", "service"]),
+  widgetSettings: defineTable({
+    organizationId: v.string(),
+    greetMessage: v.string(),
+    defaultSuggestions: v.object({
+      suggestion1: v.optional(v.string()),
+      suggestion2: v.optional(v.string()),
+      suggestion3: v.optional(v.string()),
+    }),
+    vapiSettings: v.object({
+      assistantId: v.string(),
+      phoneNumber: v.string(),
+    }),
+  }).index("by_organization_id", ["organizationId"]),
 });
