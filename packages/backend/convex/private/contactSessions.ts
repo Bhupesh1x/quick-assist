856f1b9859d29741143f4cad92abfd7e9222bbe1
Bhupesh1x <bhupeshvyas202@gmail.com>
2025-10-14 20:28:53 +0530

    Added query for fetching contact-session by conversation-id

---
+++ packages/backend/convex/private/contactSessions.ts
@@ -0,0 +1,48 @@
+import { ConvexError, v } from "convex/values";
+
+import { query } from "../_generated/server";
+
+export const getByConversationId = query({
+  args: {
+    conversationId: v.id("conversations"),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Invalid session",
+      });
+    }
+
+    const orgId = identity?.orgId as string;
+
+    if (!orgId) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Invalid session",
+      });
+    }
+
+    const conversation = await ctx.db.get(args.conversationId);
+
+    if (!conversation) {
+      throw new ConvexError({
+        code: "NOT_FOUND",
+        message: "Conversation not found",
+      });
+    }
+
+    if (conversation?.organizationId !== orgId) {
+      throw new ConvexError({
+        code: "BAD_REQUEST",
+        message: "Invalid organization id",
+      });
+    }
+
+    const contactSession = await ctx.db.get(conversation?.contactSessionId);
+
+    return contactSession;
+  },
+});
