d9c73977e9291d68fb8dc1dfa253c53a4f532cfa
Bhupesh1x <bhupeshvyas202@gmail.com>
2025-10-21 11:45:47 +0530

    Added sidebar trigger menu for mobile devices

--- apps/web/features/dashboard/components/ConversationPanel.tsx
+++ apps/web/features/dashboard/components/ConversationPanel.tsx
@@ -27,6 +27,7 @@ import {
 import { api } from "@workspace/backend/_generated/api";
 import { Skeleton } from "@workspace/ui/components/skeleton";
 import { Doc } from "@workspace/backend/_generated/dataModel";
+import { SidebarTrigger } from "@workspace/ui/components/sidebar";
 import { useInfiniteScroll } from "@workspace/ui/hooks/useInfiniteScroll";
 import { DicebarAvatar } from "@workspace/ui/components/ai/dicebar-avatar";
 import { InfiniteScrollTrigger } from "@workspace/ui/components/InfiniteScrollTrigger";
@@ -62,7 +63,8 @@ export function ConversationPanel() {
 
   return (
     <div>
-      <div className="border-b p-2">
+      <div className="border-b p-2 space-y-2 md:space-y-0">
+        <SidebarTrigger className="md:hidden" />
         <Select
           defaultValue="all"
           onValueChange={(value) =>
